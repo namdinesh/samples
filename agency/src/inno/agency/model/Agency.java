a0b4c90d961eace43179101c76bff074f64ff582
namdinesh <namdinesh@users.noreply.github.com>
2017-11-26 09:24:54 +1100

    Sample JEE Solution
    Initial commit

---
+++ agency/src/inno/agency/model/Agency.java
@@ -0,0 +1,42 @@
+package inno.agency.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "agency")
+public class Agency implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "agency_name")
+	private String agencyName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAgencyName() {
+		return agencyName;
+	}
+
+	public void setAgencyName(String agencyName) {
+		this.agencyName = agencyName;
+	}
+
+}
